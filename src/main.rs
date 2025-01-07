6a5d2acfeb5fd70d413988aa87880bf34d2a0fb6
Ilia Vorontcov <joker.pnz@gmail.com>
2025-01-07 18:00:41 -0500

    extract error logs from text file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,31 @@
 use std::fs;
 use std::io::Error;
 
-fn main() {
-    let text = fs::read_to_string("logs.txt");
+fn extract_errors(text: &str) -> Vec<String> {
+    let split_text = text.split("\n");
 
-    println!("{:#?}", text);
+    let mut results = vec![];
+
+    for line in split_text {
+        if line.starts_with("ERROR") {
+            results.push(line.to_string());
+        }
+    }
+
+    results
+}
+
+fn main() {
+    let mut error_logs = vec![];
+
+    match fs::read_to_string("logs.txt") {
+        Ok(text) => {
+            error_logs = extract_errors(text.as_str());
+        },
+        Err(error) => println!("Failed to read file: {}", error),
+    }
+
+    println!("{:#?}", error_logs);
 
     match divide(5.0, 0.0) {
         Ok(result_of_division) => {
